7d3d870dd333d82b3e8a37895a27e9d26cde5727
satoshi <angelsb94@gmail.com>
2020-08-10 17:20:53 -0500

    multiples entry points

---
+++ multi-entry-poins/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname,'src/js/index.js'),
+    precios: path.resolve(__dirname,'src/js/contacto.js'),
+    contacto: path.resolve(__dirname,'src/js/precios.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  }
+}

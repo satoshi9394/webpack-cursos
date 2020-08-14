8f16ead0217e5123a8b873d4dee7bf7a8274a2d3
satoshi <angelsb94@gmail.com>
2020-08-14 16:51:54 -0500

    plugins y loaders en webpack

---
+++ plugins/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname,'src/js/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: 'css/[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Plugins'
+    })
+  ]
+}
